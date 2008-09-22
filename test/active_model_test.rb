7366c5852068a9efce60f01028eb7de757f7c625
Matthew Bass <matt@anacreon.local>
2008-09-22 19:41:04 -0400

    added more tests

--- test/active_model_test.rb
+++ test/active_model_test.rb
@@ -12,8 +12,22 @@ class ActiveModelTest < Test::Unit::TestCase
     assert_equal "can't be blank", user.errors.on(:name)
   end
   
+  def test_initialize_attributes
+    user = User.new(:name => "John Doe")
+    assert_equal "John Doe", user.name
+  end
+  
+  def test_attribute_names
+    user = User.new
+    assert_equal ["name"], user.attribute_names
+  end
+  
   def test_attributes
     user = User.new(:name => "John Doe")
     assert_equal({"name" => "John Doe"}, user.attributes)
+  end
+  
+  def test_new_record?
+    assert !User.new.new_record?
   end
 end
